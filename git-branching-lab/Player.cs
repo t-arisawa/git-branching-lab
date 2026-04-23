86a23ace115692ffd2631e51abc938a98a2ecde3
VoidsArchive <snomonchrom@gmail.com>
2026-04-23 15:08:27 -0700

    secret

--- git-branching-lab/Player.cs
+++ git-branching-lab/Player.cs
@@ -1,8 +1,8 @@
 ﻿public class Player
 {
     // TODO: branches will replace these 0 placeholders
-    public float MoveSpeed = 0f;
-    public float JumpForce = 0f;
+    public float MoveSpeed = 999f;
+    public float JumpForce = 999f;
 
     public void Move(float horizontal)
     {
